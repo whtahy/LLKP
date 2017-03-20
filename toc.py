395c92f90a0af4910be6fc1618b14305c274ca1f
Tae Hyon Whang <whtahy@users.noreply.github.com>
2017-03-20 01:58:41 -0400

    Refactored defaults

--- toc.py
+++ toc.py
@@ -1,11 +1,18 @@
 import fileinput, re
 
-def regex(filter='LLKP.filter',
-          regex_match=r'# \d\d\d ',
-          replacement='# {} ',
-          pivot='SETTINGS',
-          index_start=0,
-          digits=3):
+defaults = {'filter': 'LLKP.filter',
+            'regex_match': r'# \d\d\d ',
+            'replacement': '# {} ',
+            'pivot': 'SETTINGS',
+            'index_start': 0,
+            'digits': 3}
+
+def regex(filter=defaults['filter'],
+          regex_match=defaults['regex_match'],
+          replacement=defaults['replacement'],
+          pivot=defaults['pivot'],
+          index_start=defaults['index_start'],
+          digits=defaults['digits']):
     with fileinput.FileInput(filter, inplace=True) as text:
         pattern = re.compile(regex_match)
         restart = False
